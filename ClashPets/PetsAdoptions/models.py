37915434585766f72cf21e0895614823fa9bbb6e
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-05-09 21:45:27 +0530

    models.py

--- ClashPets/PetsAdoptions/models.py
+++ ClashPets/PetsAdoptions/models.py
@@ -10,7 +10,7 @@ class Pet(models.Model):
     sex = models.CharField(max_length=1,choices=SEX_CHOICES,blank=True)
     submission_date = models.DateField()
     age = models.IntegerField(null=True)
-    vaccination = models.ManyToManyField('Vaccine',blank=True)
+    vaccinations = models.ManyToManyField('Vaccine',blank=True)
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
